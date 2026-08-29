0dcd772a6d5d899dfb6eb73d0e69333a60c8914e
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2026-08-30 00:52:09 +0300

    create seeders for patterns that used to extract title, price, amd image_url

--- product-backend/database/migrations/2026_08_29_020112_create_patterns_table.php
+++ product-backend/database/migrations/2026_08_29_020112_create_patterns_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('patterns', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('for');
             $table->timestamps();
         });
     }
